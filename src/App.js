dd59afa554673e862520dcfdf850939688147f9b
nathanjukes <njukes1@gmail.com>
2023-10-22 23:35:16 +0100

    setup react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" Component={Navbar} />
           <Route path="/navbar" Component={Navbar} />
+          <Route path="/signup" Component={Signup} />
         </Routes>
       </Router>
     </div>
