33ada96de80d1216fb80a8444fb2af040a36bf11
Nathan Jukes <njukes1@gmail.com>
2023-11-27 15:33:24 +0000

    add org name to navbar

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AuthProvider } from "../../context/AuthProvider";
 import User from "../Auth/User";
 import { Link } from "react-router-dom";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { useState } from "react";
+
+const ORGS_URL = "/orgs/me";
 
 const Navbar = () => {
+  const [orgInfo, setOrgInfo] = useState(null);
+  const axiosPrivate = useAxiosPrivate();
+
+  useEffect(() => {
+    const getOrgInfo = async () => {
+      try {
+        const response = await axiosPrivate.get(ORGS_URL);
+        setOrgInfo(response.data);
+      } catch (error) {
+        console.error("Error getting org info:", error);
+      }
+    };
+
+    getOrgInfo();
+  }, []);
+
+  if (!orgInfo) {
+    return <nav></nav>;
+  }
+
   return (
     <nav class="bg-navBarWhite border-neutral-500 p-8 text-black text-center shadow-neutral-400">
       <h1 class="text-4xl font-bold leading-none tracking-tight text-center">
-        Cisco's Dashboard
+        {orgInfo.name} 's Dashboard
       </h1>
     </nav>
   );
