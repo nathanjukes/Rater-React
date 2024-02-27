23f640d9a474bceac3fcd67877cddc53b5f95db0
nathanjukes <njukes1@gmail.com>
2024-02-27 12:28:59 +0000

    github pages

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import SignUpPublic from "./components/Public/SignUpPublic";
 function App() {
   return (
     <div>
-      <Router>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/signup" element={<SignUpPublic />} />
@@ -30,7 +30,7 @@ function App() {
             element={<Dashboard startPage="Overview" />}
           />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
